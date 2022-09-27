7a52cb03e92c4d625a2d6d964ff64cb7b2f9aaba
ThijsDeR <rech0004@hz.nl>
2022-09-27 21:08:24 +0200

    Data Showcase

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [count, setCount] = useState(0)
+  const [importantCalc, setImportantCalc] = useState(0)
+  const [name, setName] = useState("")
+
+  useEffect(() => {
+    setImportantCalc(count * count)
+  }, [count])
+
   return (
     <div>
-      
+      <div>
+        <h2>Count: {count}</h2>
+        <h2>Important Calc: {importantCalc}</h2>
+        <button onClick={() => {setCount(count + 1)}}>Moreee</button>
+        <button onClick={() => {setCount(count - 1)}}>Lesssss</button>
+      </div>
+
+      <div>
+        <h2>Name: {name}</h2>
+        <input onChange={(ev) => setName(ev.currentTarget.value)}></input>
+      </div>
+
     </div>
   )
 }
